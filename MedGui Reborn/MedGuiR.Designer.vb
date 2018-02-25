c83d3c42fe8bcab172f042cbca280740c4f64a22
vincenzo <speedvicio@hotmail.com>
2018-02-25 19:45:22 +0100

    Added SDL_SCANCODE value on keyassign module. Added opengl missing value on video driver.

--- MedGui Reborn/MedGuiR.Designer.vb
+++ MedGui Reborn/MedGuiR.Designer.vb
@@ -1774,7 +1774,7 @@ Partial Class MedGuiR
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(91, 13)
         Me.Label6.TabIndex = 30
-        Me.Label6.Text = "MedGuiR v.0.093"
+        Me.Label6.Text = "MedGuiR v.0.094"
         '
         'Label7
         '
