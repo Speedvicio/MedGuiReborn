12a1a80b77834452f94d011ea2356cc0634bd064
vincenzo <speedvicio@hotmail.com>
2018-10-20 17:55:34 +0200

    - TGDBSetting (2)

--- MedGui Reborn/TGBSettings.Designer.vb
+++ MedGui Reborn/TGBSettings.Designer.vb
@@ -52,13 +52,13 @@ Partial Class TGBSettings
         Me.Panel1.Enabled = False
         Me.Panel1.Location = New System.Drawing.Point(12, 35)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(185, 111)
+        Me.Panel1.Size = New System.Drawing.Size(212, 111)
         Me.Panel1.TabIndex = 0
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(13, 83)
+        Me.Label4.Location = New System.Drawing.Point(67, 84)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(55, 13)
         Me.Label4.TabIndex = 40
@@ -67,7 +67,7 @@ Partial Class TGBSettings
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(13, 59)
+        Me.Label3.Location = New System.Drawing.Point(72, 57)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(50, 13)
         Me.Label3.TabIndex = 39
@@ -76,7 +76,7 @@ Partial Class TGBSettings
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(13, 35)
+        Me.Label2.Location = New System.Drawing.Point(81, 30)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(41, 13)
         Me.Label2.TabIndex = 38
@@ -85,7 +85,7 @@ Partial Class TGBSettings
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 11)
+        Me.Label1.Location = New System.Drawing.Point(61, 3)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(61, 13)
         Me.Label1.TabIndex = 37
@@ -95,7 +95,7 @@ Partial Class TGBSettings
         '
         Me.PictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.PictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox4.Location = New System.Drawing.Point(145, 84)
+        Me.PictureBox4.Location = New System.Drawing.Point(128, 84)
         Me.PictureBox4.Name = "PictureBox4"
         Me.PictureBox4.Size = New System.Drawing.Size(23, 21)
         Me.PictureBox4.TabIndex = 36
@@ -105,7 +105,7 @@ Partial Class TGBSettings
         '
         Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox2.Location = New System.Drawing.Point(145, 30)
+        Me.PictureBox2.Location = New System.Drawing.Point(128, 30)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(23, 21)
         Me.PictureBox2.TabIndex = 35
@@ -115,7 +115,7 @@ Partial Class TGBSettings
         '
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox1.Location = New System.Drawing.Point(145, 3)
+        Me.PictureBox1.Location = New System.Drawing.Point(128, 3)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(23, 21)
         Me.PictureBox1.TabIndex = 34
@@ -125,7 +125,7 @@ Partial Class TGBSettings
         '
         Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox3.Location = New System.Drawing.Point(145, 57)
+        Me.PictureBox3.Location = New System.Drawing.Point(128, 57)
         Me.PictureBox3.Name = "PictureBox3"
         Me.PictureBox3.Size = New System.Drawing.Size(23, 21)
         Me.PictureBox3.TabIndex = 33
@@ -134,7 +134,7 @@ Partial Class TGBSettings
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBox1.Location = New System.Drawing.Point(84, 12)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(68, 17)
         Me.CheckBox1.TabIndex = 0
@@ -145,7 +145,7 @@ Partial Class TGBSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(209, 153)
+        Me.ClientSize = New System.Drawing.Size(236, 153)
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
--- MedGui Reborn/TGBSettings.vb
+++ MedGui Reborn/TGBSettings.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.IO
+Imports Newtonsoft.Json
 
 Public Class TGBSettings
+
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
         If CheckBox1.Checked = True Then
             Panel1.Enabled = True
@@ -13,23 +15,26 @@ Public Class TGBSettings
 
     Private Sub TGBSettings_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Icon = gIcon
+        F1 = Me
+        CenterForm()
     End Sub
+
     Public Function MakeTGDBList(ByVal APIvalue As String)
-        Dim myWebClient As New Net.WebClient()
         Dim SplittedScrapedValue() As String
         Dim ResultScaped As String
-        Dim myStream As Stream = myWebClient.OpenRead("https://api.thegamesdb.net/" & APIvalue & "?apikey=" & VSTripleDES.DecryptData("WGjcjnPpu1N7Cj92IF4kIiHtXEd5iNXtaZWpDkM5FVIwZuE2Kpo8R5KzvfENFA8kWmEGOig1hY30hlgoajQ+JjL+Zyv5rmRS+FPemgiaKTTMsGDF4jNR0W1rCBblcD9p6CrUK9MH7YKlexs1HkfrqTlyGKzDLlla1vBfFiI1gmp9haPCMNH84Q=="))
-        Dim sr As New StreamReader(myStream)
+        Dim JsonT As String = New Net.WebClient().DownloadString("https://api.thegamesdb.net/" & APIvalue & "?apikey=" & VSTripleDES.DecryptData("WGjcjnPpu1N7Cj92IF4kIiHtXEd5iNXtaZWpDkM5FVIwZuE2Kpo8R5KzvfENFA8kWmEGOig1hY30hlgoajQ+JjL+Zyv5rmRS+FPemgiaKTTMsGDF4jNR0W1rCBblcD9p6CrUK9MH7YKlexs1HkfrqTlyGKzDLlla1vBfFiI1gmp9haPCMNH84Q=="))
 
-        Dim salva As New IO.StreamWriter(Application.StartupPath & "\" & APIvalue) 'salva il file'
-        salva.WriteLine(sr.ReadToEnd.Split(vbCrLf))
-        salva.Close()
+        If File.Exists(MedExtra & "\Plugins\db\TGDB") = False Then
+            Directory.CreateDirectory(MedExtra & "\Plugins\db\TGDB")
+        End If
+
+        File.WriteAllText(MedExtra & "\Plugins\db\TGDB\" & APIvalue, JsonConvert.SerializeObject(JsonConvert.DeserializeXmlNode(JsonT, "Root"), Formatting.Indented))
 
         Dim oFile As System.IO.File
         Dim oRead As System.IO.StreamReader
 
         Try
-            oRead = oFile.OpenText(Application.StartupPath & "\" & APIvalue)
+            oRead = oFile.OpenText(MedExtra & "\Plugins\db\TGDB\" & APIvalue)
 
             While oRead.Peek <> -1
                 SplittedScrapedValue = Split(oRead.ReadLine(), ":")
@@ -38,17 +43,22 @@ Public Class TGBSettings
                     Case """id"""
                         ResultScaped += Replace(SplittedScrapedValue(1).Trim, ",", " | ")
                     Case """name"""
-                        ResultScaped += SplittedScrapedValue(1).Trim & vbCrLf
+                        ResultScaped += SplittedScrapedValue(1).Trim
+                        If ResultScaped.Substring(ResultScaped.Length - 1, 1) = "," Then
+                            ResultScaped = ResultScaped.Substring(0, ResultScaped.Length - 1)
+                        End If
+                        ResultScaped += vbCrLf
                 End Select
+
+                ResultScaped = Replace(ResultScaped, """", "")
             End While
 
-            Dim wfile As System.IO.StreamWriter
-            wfile = My.Computer.FileSystem.OpenTextFileWriter(Application.StartupPath & "\" & APIvalue & ".txt", False)
-            wfile.Write(ResultScaped)
-            wfile.Close()
+            File.WriteAllText(MedExtra & "\Plugins\db\TGDB\" & APIvalue & ".txt", ResultScaped)
         Catch ex As Exception
         Finally
-            sr.Close()
+            oRead.Close()
+            File.Delete(MedExtra & "\Plugins\db\TGDB\" & APIvalue)
+            MsgBox(APIvalue & " Updated!", MsgBoxStyle.Information + vbOKOnly, "Update Done...")
         End Try
     End Function
 
@@ -67,4 +77,5 @@ Public Class TGBSettings
     Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
         MakeTGDBList("Publishers")
     End Sub
+
 End Class
