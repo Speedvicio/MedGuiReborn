9594a70de4e7db6628c4c7608efdbe150f196e18
vincenzo <speedvicio@hotmail.com>
2019-01-21 19:04:36 +0100

    - Implemented , Added, Remove , Modify Cheat

--- MedGui Reborn/Mcheat.vb
+++ MedGui Reborn/Mcheat.vb
@@ -3,6 +3,8 @@
 Public Class Mcheat
     Dim TypeCheat, CheatActive, LittleEndian, ByteLenght, CodeAdress, ByteValue, CheatName, CheatConsole As String
     Dim TWriteRAM As Boolean = True
+    Dim ControlCheatPresence As Integer
+    Dim DoesentPrepare As Boolean = False
 
     Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
         TextBox3.MaxLength = (NumericUpDown1.Value * 2) + 1
@@ -42,8 +44,10 @@ Public Class Mcheat
                     TextBox3.Text = SplitAdress(1)
                 End If
             Case Else
+
                 TextBox1.Text = SplitAdress(0)
-                TextBox3.Text = SplitAdress(1)
+                If SplitAdress.Length > 1 Then TextBox3.Text = SplitAdress(1)
+
         End Select
         TextBox1.Text = TextBox1.Text.PadLeft(8, "0")
     End Function
@@ -56,7 +60,10 @@ Public Class Mcheat
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PrepareCodeforMednafen()
+    End Sub
 
+    Private Sub PrepareCodeforMednafen()
         If TWriteRAM = False Then AnalizeRAWCode(TextBox1.Text)
 
         SetCodeMode()
@@ -66,7 +73,7 @@ Public Class Mcheat
 
     Private Function FormatText(TextValue As String, MTLenght As Integer) As String
         TextValue = TextValue.PadLeft(MTLenght, "0")
-        If TextValue.Length > MTLenght - 1 Then TextValue = TextValue.Remove(0, 1)
+        If TextValue.Length > MTLenght Then TextValue = TextValue.Remove(0, 1)
         FormatText = TextValue
     End Function
 
@@ -103,6 +110,105 @@ Public Class Mcheat
 
     End Sub
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If ListBox1.SelectedIndex < 0 Then Exit Sub
+        WorkingWithCheat(ListBox1.SelectedItem.ToString & vbLf & vbLf, "", False)
+        If ListBox1.Items.Count = 0 Then
+            WorkingWithCheat(vbLf & "[" & TextBox2.Text & "] " & Label7.Text.Trim & vbLf, "", False)
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If ListBox1.SelectedIndex < 0 Then Exit Sub
+        PrepareCodeforMednafen()
+        WorkingWithCheat(ListBox1.SelectedItem.ToString, Label11.Text, False)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PrepareCodeforMednafen()
+
+        Dim cheatpath As String = Path.Combine(MedGuiR.TextBox4.Text, "cheats\" & CheatConsole & ".cht")
+
+        If File.Exists(cheatpath) = False Then
+            File.Create(cheatpath).Dispose()
+        End If
+
+        ListBox1.Items.Clear()
+        ParseCht()
+
+        If ControlCheatPresence = 1 Then Exit Sub
+
+        If ListBox1.Items.Count < 1 Then
+            Dim ExNovo As String = vbLf & "[" & TextBox2.Text & "] " & Label7.Text.Trim & vbLf &
+                Label11.Text.Trim & vbLf & vbLf
+
+            'Dim myFile As New FileInfo(cheatpath)
+
+            'If myFile.Length < 40 Then
+            WorkingWithCheat("", ExNovo, True)
+            'Else
+            'WorkingWithCheat("", ExNovo, True)
+            'End If
+
+        Else
+            If ListBox1.SelectedIndex < 0 Then DoesentPrepare = True
+            ListBox1.SelectedIndex = ListBox1.Items.Count - 1
+            If ListBox1.SelectedItem.ToString.Trim = Label11.Text.Trim Then Exit Sub
+            WorkingWithCheat(ListBox1.SelectedItem.ToString.Trim, ListBox1.SelectedItem.ToString.Trim & vbLf & vbLf & Label11.Text.Trim & vbLf, False)
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.Items.Count < 1 Or ListBox1.SelectedIndex < 0 Then Exit Sub
+
+        RadioButton6.Checked = True
+        Dim RetrieveCheatValue() As String = ListBox1.SelectedItem.ToString.Split(" ")
+
+        Select Case RetrieveCheatValue(0)
+            Case "R"
+                RadioButton1.Checked = True
+            Case "A"
+                RadioButton2.Checked = True
+            Case "T"
+                RadioButton3.Checked = True
+            Case "S"
+                RadioButton4.Checked = True
+            Case "C"
+                RadioButton5.Checked = True
+        End Select
+
+        Select Case RetrieveCheatValue(1)
+            Case "A"
+                CheckBox1.Checked = True
+            Case "I"
+                CheckBox1.Checked = False
+        End Select
+
+        NumericUpDown1.Value = Val(RetrieveCheatValue(2))
+
+        Select Case RetrieveCheatValue(3)
+            Case "B"
+                CheckBox2.Checked = False
+            Case "L"
+                CheckBox2.Checked = True
+        End Select
+
+        TextBox1.MaxLength = RetrieveCheatValue(5).Length + 1
+        TextBox1.Text = RetrieveCheatValue(5)
+
+        TextBox3.MaxLength = RetrieveCheatValue(6).Length + 1
+        TextBox3.Text = RetrieveCheatValue(6)
+
+        Dim cheatname As String
+        For i = 7 To RetrieveCheatValue.Length - 1
+            cheatname += RetrieveCheatValue(i) & " "
+        Next
+
+        If cheatname Is Nothing = False Then TextBox4.Text = cheatname.Trim
+
+        If DoesentPrepare = False Then PrepareCodeforMednafen()
+    End Sub
+
     Private Sub Mcheat_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ResetAll()
         CheatConsole = LCase(MedGuiR.DataGridView1.CurrentRow.Cells(6).Value)
@@ -117,6 +223,7 @@ Public Class Mcheat
 
         MedGuiR.SetSpecialModule()
         If CheatConsole = "snes" And MedGuiR.tpce = "_faust" Then
+            CheatConsole = "snes_faust"
             TextBox2.Text = r_sha.Substring(0, 32)
         Else
             TextBox2.Text = r_md5
@@ -166,6 +273,7 @@ Public Class Mcheat
     End Sub
 
     Private Sub ParseCht()
+        ControlCheatPresence = 0
         Dim readText As String = File.ReadAllText(Path.Combine(MedGuiR.TextBox4.Text, "cheats\" & CheatConsole & ".cht"))
         Dim DeatilCheat() As String = readText.Split("[")
 
@@ -178,60 +286,14 @@ Public Class Mcheat
             End If
         Next
 
+        If SplitCheat Is Nothing Then Exit Sub
+
         For i = 1 To SplitCheat.Length - 1
             If SplitCheat(i).Trim = "" Then Continue For
             ListBox1.Items.Add(SplitCheat(i).ToString)
+            If SplitCheat(i).ToString.Trim = Label11.Text.Trim Then ControlCheatPresence = 1
         Next
 
-    End Sub
-
-    Private Sub ListBox1_DoubleClick(sender As Object, e As EventArgs) Handles ListBox1.DoubleClick
-        If ListBox1.Items.Count < 1 Then Exit Sub
-
-        RadioButton6.Checked = True
-        Dim RetrieveCheatValue() As String = ListBox1.SelectedItem.ToString.Split(" ")
-
-        Select Case RetrieveCheatValue(0)
-            Case "R"
-                RadioButton1.Checked = True
-            Case "A"
-                RadioButton2.Checked = True
-            Case "T"
-                RadioButton3.Checked = True
-            Case "S"
-                RadioButton4.Checked = True
-            Case "C"
-                RadioButton5.Checked = True
-        End Select
-
-        Select Case RetrieveCheatValue(1)
-            Case "A"
-                CheckBox1.Checked = True
-            Case "I"
-                CheckBox1.Checked = False
-        End Select
-
-        NumericUpDown1.Value = Val(RetrieveCheatValue(2))
-
-        Select Case RetrieveCheatValue(3)
-            Case "B"
-                CheckBox2.Checked = False
-            Case "L"
-                CheckBox2.Checked = True
-        End Select
-
-        TextBox1.MaxLength = RetrieveCheatValue(5).Length + 1
-        TextBox1.Text = RetrieveCheatValue(5)
-
-        TextBox3.MaxLength = RetrieveCheatValue(6).Length + 1
-        TextBox3.Text = RetrieveCheatValue(6)
-
-        Dim cheatname As String
-        For i = 7 To RetrieveCheatValue.Length - 1
-            cheatname += RetrieveCheatValue(i) & " "
-        Next
-
-        TextBox4.Text = cheatname.Trim
     End Sub
 
     Private Sub ResetAll()
@@ -249,4 +311,22 @@ Public Class Mcheat
         Label11.Text = "Result Code"
         Label11.Left = (Me.Width / 2) - (Label11.Width / 2)
     End Sub
+
+    Private Function WorkingWithCheat(OriginalString As String, StringChange As String, AppendTxt As Boolean)
+        RadioButton1.Checked = True
+
+        Dim txtcheat = My.Computer.FileSystem.ReadAllText(Path.Combine(MedGuiR.TextBox4.Text, "cheats\" & CheatConsole & ".cht"))
+
+        If AppendTxt = False Then
+            txtcheat = txtcheat.Replace(OriginalString, StringChange)
+        Else
+            txtcheat = StringChange
+        End If
+        My.Computer.FileSystem.WriteAllText(Path.Combine(MedGuiR.TextBox4.Text,
+            "cheats\" & CheatConsole & ".cht"), txtcheat, AppendTxt)
+
+        DoesentPrepare = False
+        ListBox1.Items.Clear()
+        ParseCht()
+    End Function
 End Class
