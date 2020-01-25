3001719d36a172aec0b23a175a11803489d58b0b
Speedvicio <speedvicio@hotmail.com>
2020-01-25 17:37:47 +0100

    - Ready for v.0.110

--- MedGui Reborn/MedGuiR.vb
+++ MedGui Reborn/MedGuiR.vb
@@ -1105,7 +1105,7 @@ Public Class MedGuiR
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         rDes = "Select Mednafen Path"
         yPath()
-        If rPath <> "" Then TextBox4.Text = rPath : exist_Mednafen() : 
+        If rPath <> "" Then TextBox4.Text = rPath : exist_Mednafen() :
         MednafenV()
     End Sub
 
