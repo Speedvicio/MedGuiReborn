40d9351e16f6e74c621d119e92e6dbce0033c0d2
Speedvicio <speedvicio@hotmail.com>
2020-05-07 02:27:55 +0200

    - Ready for  v.0.113

--- MedGui Reborn/MedGuiR.Designer.vb
+++ MedGui Reborn/MedGuiR.Designer.vb
@@ -2384,7 +2384,7 @@ Partial Class MedGuiR
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(91, 13)
         Me.Label6.TabIndex = 30
-        Me.Label6.Text = "MedGuiR v.0.112"
+        Me.Label6.Text = "MedGuiR v.0.113"
         '
         'Label7
         '
